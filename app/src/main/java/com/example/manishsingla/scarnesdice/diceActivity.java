58e1f616e3b3d63a55c9f8af949a14d333f29221
manish singla <201452018@iiitvadodara.ac.in>
2016-09-25 12:51:46 +0530

    Initial commit

---
+++ app/src/main/java/com/example/manishsingla/scarnesdice/diceActivity.java
@@ -0,0 +1,13 @@
+package com.example.manishsingla.scarnesdice;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class diceActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dice);
+    }
+}
